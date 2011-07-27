71e89c3323c22e8bbea78d92db03d6218c525538
phersho@gmail.com <phersho@gmail.com@a1e7415e-3336-7f9f-a9e9-be1c856ffa9e>
2011-07-27 04:05:53 +0000

    Se remueven usos de espacios de nombres sin utilizar.

--- ProjectsAndTendering/0.0.A/Samsara.ProjectsAndTendering.Core/Parameters/SearchTendersParameters.cs
+++ ProjectsAndTendering/0.0.A/Samsara.ProjectsAndTendering.Core/Parameters/SearchTendersParameters.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Samsara.ProjectsAndTendering.Core.Enums;
 
 namespace Samsara.ProjectsAndTendering.Core.Parameters
